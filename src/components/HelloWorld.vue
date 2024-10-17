6f0cdc8e0d851bbb8cae2a20e11c3fa41b443054
churuizramos <ruizramos04@outlook.com>
2024-10-16 20:13:16 -0400

    rebuild

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+// This file is only for testing to see whether it is rendering correctly.
+// Remove it from the MainPage.vue file in order to start working.
+
+<template>
+    <div class="text-center">
+        <h1 class="text-2xl">Hello World!</h1>
+    </div>
+</template>
