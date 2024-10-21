c657217360896d12b45b02d5227e437dc27848d8
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-10-21 13:57:08 -0400

    created a temporary footer

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="flex flex-row gap-4 p-3 justify-center bg-amber-300">
+        <p>Cooking App</p>
+        <a href="https://github.com/churuizramos">churuizramos</a>
+        <a href="https://github.com/mkndiaye">mkndiaye</a>
+        <a href="https://github.com/GreyNeedham">GreyNeedham</a>
+    </footer>
+</template>
