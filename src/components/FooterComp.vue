5bf30e7fe4dec67a6799cea6080b38e09e44dbe9
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-10-21 13:58:23 -0400

    Added link to the codebase

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="flex flex-row gap-4 p-3 justify-center bg-amber-300">
-        <p>Cooking App</p>
+        <a href="https://github.com/churuizramos">Cooking App</a>
         <a href="https://github.com/churuizramos">churuizramos</a>
         <a href="https://github.com/mkndiaye">mkndiaye</a>
         <a href="https://github.com/GreyNeedham">GreyNeedham</a>
