374ae23e95b0cc22ea468883b571a77e473e63ff
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-10-21 17:37:59 -0400

    Added place holders for carousels and applied main nav and footer

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,9 +1,22 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import FooterComp from './components/FooterComp.vue';
 </script>
 
 <template>
     <NavBar />
+    <div class="p-6 bg-amber-300 flex flex-col gap-6" id="main-page-results">
+      <div id="carousel-1">
+        <p>Carousel 1</p>
+      </div>
+      <div id="carousel-1">
+        <p>Carousel 2</p>
+      </div>
+      <div id="carousel-1">
+        <p>Carousel 3</p>
+      </div>
+    </div>
+    <FooterComp />
 </template>
 
 <style scoped>
