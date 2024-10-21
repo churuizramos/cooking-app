f62ad26c8d9503081d1519cfaf6c379e6fef1d7c
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-10-21 13:56:47 -0400

    updated Navbar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex flex-row gap-4 bg-yellow-100 p-3">
+    <nav class="flex flex-row gap-4 bg-amber-300 p-3">
         <button>Home</button>
         <input class="p-2 w-full rounded-full" type="text" id="searchBox" placeholder="Search recipes"/>
         <button><img src="/public/icons8-user-40.png" alt="User temporary icon."/></button>
