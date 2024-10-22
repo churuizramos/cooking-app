71ae9c68b4078ed4e0f930aa5591064e9e3a3ca7
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-10-22 14:40:32 -0400

    added the new banner to the main app

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,24 +1,28 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import FooterComp from './components/FooterComp.vue';
+import Banner from './components/Banner.vue';
+
+import { ref } from 'vue';
+
+const userFirstName = ref('John Doe')
 </script>
 
 <template>
-    <NavBar />
-    <div class="p-6 bg-amber-300 flex flex-col gap-6" id="main-page-results">
-      <div id="carousel-1">
-        <p>Carousel 1</p>
-      </div>
-      <div id="carousel-1">
-        <p>Carousel 2</p>
-      </div>
-      <div id="carousel-1">
-        <p>Carousel 3</p>
-      </div>
+  <NavBar />
+  <Banner>
+    <span>{{userFirstName}}</span>
+  </Banner>
+  <div class="p-6 bg-amber-300 flex flex-col gap-10 text-3xl" id="main-page-results">
+    <div id="carousel-1">
+      <p>Carousel 1</p>
     </div>
-    <FooterComp />
+    <div id="carousel-1">
+      <p>Carousel 2</p>
+    </div>
+    <div id="carousel-1">
+      <p>Carousel 3</p>
+    </div>
+  </div>
+  <FooterComp />
 </template>
-
-<style scoped>
-
-</style>
