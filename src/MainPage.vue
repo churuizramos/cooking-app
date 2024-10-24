2188a0820a4bbbdd0c60b52ea89d937808b780c6
churuizramos <ruizramos04@outlook.com>
2024-10-24 13:19:20 -0400

    finsihed with the main-page

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,28 +1,36 @@
 <script setup>
-import NavBar from './components/NavBar.vue';
-import FooterComp from './components/FooterComp.vue';
-import Banner from './components/Banner.vue';
+import NavBar from "./components/NavBar.vue";
+import FooterComp from "./components/FooterComp.vue";
+import Banner from "./components/Banner.vue";
 
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const userFirstName = ref('John Doe')
+const userFirstName = ref("John Doe");
 </script>
 
 <template>
-  <NavBar />
-  <Banner>
-    <span>{{userFirstName}}</span>
-  </Banner>
-  <div class="p-6 bg-amber-300 flex flex-col gap-10 text-3xl" id="main-page-results">
-    <div id="carousel-1">
-      <p>Carousel 1</p>
-    </div>
-    <div id="carousel-1">
-      <p>Carousel 2</p>
-    </div>
-    <div id="carousel-1">
-      <p>Carousel 3</p>
-    </div>
-  </div>
-  <FooterComp />
+    <NavBar />
+    <Banner>
+        <span>{{ userFirstName }}</span>
+    </Banner>
+    <main
+        class="flex flex-col gap-6 p-6 text-3xl bg-white"
+        id="main-page-results"
+    >
+        <div>
+            <h1>Breakfast</h1>
+            <div id="carousel_1" class="flex flex-row gap-4"></div>
+        </div>
+
+        <div>
+            <h1>Drinks</h1>
+            <div id="carousel_2" class="flex flex-row gap-4"></div>
+        </div>
+
+        <div>
+            <h1>Late Treats</h1>
+            <div id="carousel-_3" class="flex flex-row gap-4"></div>
+        </div>
+    </main>
+    <FooterComp />
 </template>
